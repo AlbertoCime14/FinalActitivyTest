c06a41082ca083f2625c998846dd387d903fd076
Alberto Cime <alberto_cime@epam.com>
2022-08-25 21:22:28 -0500

    test 11

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM python:3.9
 
+ENV PYTHONUNBUFFERED True
 
+COPY requirements.txt ./
+RUN set -ex; \
+    pip install -r requirements.txt
 
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
-
-RUN pip3 --no-cache-dir install -r requirements.txt
 
 CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from flask import Flask
 app = Flask(__name__)
 
 def init_unix_connection_engine():
-    db_user = 'admin'
-    db_pass = 'test123'
-    db_name = 'rookie'
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
     
     db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
     
-    cloud_sql_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
     
     pool = sqlalchemy.create_engine(
         sqlalchemy.engine.url.URL(
@@ -23,33 +23,7 @@ def init_unix_connection_engine():
                 db_socket_dir,
                 cloud_sql_connection_name)
             }
-        ),
-        pool_size=5,
-        # Temporarily exceeds the set pool_size if no connections are available.
-        max_overflow=2,
-        # The total number of concurrent connections for your application will be
-        # a total of pool_size and max_overflow.
-        # [END cloud_sql_postgres_sqlalchemy_limit]
-
-        # [START cloud_sql_postgres_sqlalchemy_backoff]
-        # SQLAlchemy automatically uses delays between failed connection attempts,
-        # but provides no arguments for configuration.
-        # [END cloud_sql_postgres_sqlalchemy_backoff]
-
-        # [START cloud_sql_postgres_sqlalchemy_timeout]
-        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-        # new connection from the pool. After the specified amount of time, an
-        # exception will be thrown.
-        pool_timeout=30,  # 30 seconds
-        # [END cloud_sql_postgres_sqlalchemy_timeout]
-
-        # [START cloud_sql_postgres_sqlalchemy_lifetime]
-        # 'pool_recycle' is the maximum number of seconds a connection can persist.
-        # Connections that live longer than the specified amount of time will be
-        # re-established
-        pool_recycle=1800,  # 30 minutes
-        # [END cloud_sql_postgres_sqlalchemy_lifetime]
-        # [END_EXCLUDE]
+        )
     )
     
     pool.dialect.description_encoding = None
@@ -61,7 +35,7 @@ def main():
     db = init_unix_connection_engine()
 
     with db.connect() as conn:
-        result = conn.execute().fetchall()
+        result = conn.execute("SELECT * from land_registry_price_paid_uk where postcode = 'E15 3AR';").fetchall()
 
     return str(result)
 
